5aeb8f295439d7dd5773473219f5b2bc2f5eb136
Dan Rusu <dan.rusu@rms.com>
2019-12-12 19:00:28 +0200

    added selenium startup package

---
+++ java/basics/src/selenium/SeleniumDriver.java
@@ -0,0 +1,43 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+public class SeleniumDriver {
+
+    private static String getChromeDriverPath() {
+        return Paths.get(
+                System.getProperty("user.dir"),
+                "webDrivers",
+                "chromedriver.exe"
+        ).toString();
+    }
+
+    public static WebDriver startChromeDriver(){
+        System.setProperty("webdriver.chrome.driver", getChromeDriverPath());
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(Arrays.asList("--start-maximized"));
+
+        return new ChromeDriver(options);
+    }
+
+    public static void setImplicitWait(WebDriver driver, long milliSecondsTimeout){
+        driver.manage().timeouts().implicitlyWait(
+                milliSecondsTimeout,
+                TimeUnit.MILLISECONDS
+        );
+    }
+
+    public static void waitFor(long milliSecondsTimeout){
+        try {
+            Thread.sleep(milliSecondsTimeout);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ java/basics/src/selenium/Test.java
@@ -0,0 +1,29 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Start the Selenium Chrome driver");
+        WebDriver driver = SeleniumDriver.startChromeDriver();
+        SeleniumDriver.setImplicitWait(driver, 5000);
+
+        driver.navigate().to("http://www.danrusu.ro");
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("First url: " + currentUrl);
+
+        WebElement link = driver.findElement(By.linkText("Play smart with the backend"));
+        link.click();
+
+        SeleniumDriver.waitFor(2000);
+        currentUrl = driver.getCurrentUrl();
+        System.out.println("Second url: " + currentUrl);
+
+        System.out.println("Close driver");
+        driver.quit();
+    }
+}
